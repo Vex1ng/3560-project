0ed9a7b4d1bfd9d1f92051d6501e02448686de01
blbergo <bryan.l.bergo@gmail.com>
2024-10-21 15:27:11 -0700

    supabase

---
+++ src/config/constants.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = process.env.SUPABASE_URL || "missing SUPABASE_URL";
+export const SUPABASE_ANON_KEY =
+  process.env.SUPABASE_ANON_KEY || "missing SUPABASE_ANONKEY";
---
+++ src/utils/supabase.ts
@@ -0,0 +1,4 @@
+import { SUPABASE_ANON_KEY, SUPABASE_URL } from "@/config/constants";
+import { createClient } from "@supabase/supabase-js";
+
+export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
