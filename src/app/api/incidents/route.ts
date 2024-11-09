8d127a57d5825b0d0117dbdba050f83b2262fe2e
blbergo <bryan.l.bergo@gmail.com>
2024-11-09 10:42:36 -0800

    cleanup + placeholder file

---
+++ src/app/api/incidents/route.ts
@@ -0,0 +1,18 @@
+// get incidents
+export async function GET(request: Request) {
+    
+}
+
+// create them
+export async function POST(request: Request) {
+
+}
+
+// update them
+export async function PUT(request: Request) {
+
+
+}
+
+
+// no delete since prof doesn't want that
