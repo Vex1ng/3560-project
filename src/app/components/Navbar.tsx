be38fb0f579b906832eb89a554564a45dcca7879
blbergo <bryan.l.bergo@gmail.com>
2024-11-15 10:33:29 -0800

    navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import NavbarItem from "./NavbarItem";
+
+export default function Navbar() {
+  return (
+    <div className="flex flex-row justify-between w-full">
+      <p>Logo</p>
+      <div className="flex flex-row gap-x-20 items-center">
+        <NavbarItem href="/" text="Active Reports" />
+        <NavbarItem href="/reports" text="Public Reports" />
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex flex-col px-[100px] pt-20 bg-white">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
