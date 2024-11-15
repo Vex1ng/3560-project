9ae3fdf919ce257fc3c3eaff768b40404684ce6e
blbergo <bryan.l.bergo@gmail.com>
2024-11-15 11:54:10 -0800

    reports tabel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import Button from "./components/Button";
 import Card from "./components/Card";
 import Dropdown from "./components/Dropdown";
 import StatCard from "./components/StatCard";
+import Table, { COLOR_CODES, TableProps } from "./components/Table";
 
 export default function Home() {
+  const data: TableProps["data"] = [{
+    id: "000111",
+    type: {
+      label: "Welfare Check",
+      colorcode: COLOR_CODES.Warning
+    },
+    priority: 3,
+    assignedUnits: "Suarez, O'Brian",
+    dateReported: "10-18-24 5:30pm"
+  }];
   return (
     <>
       <div className="flex flex-col pt-[30px] gap-y-[30px]">
@@ -14,6 +26,13 @@ export default function Home() {
           <StatCard title="Available Units" data={100} />
           <StatCard title="Reports Submitted" data={30} />
         </div>
+        <Card>
+          <div className="flex flex-row w-full justify-between">
+            <p className="text-2xl font-bold">Open Cases</p>
+            <Button label="Add Report" />
+          </div>
+          <Table data={data} />
+        </Card>
       </div>
     </>
   );
