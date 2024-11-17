68b64bacb0e28417127df222374b9e285497f3cc
Inevitabby <161525738+Inevitabby@users.noreply.github.com>
2024-11-16 16:13:17 -0800

    fix: Use dispatch_system instead of mysql

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 export const MYSQL_HOST = "localhost";
 export const MYSQL_USER = "root";
 export const MYSQL_PASSWORD = "mysql";
-export const MYSQL_DATABASE = "mysql";
+export const MYSQL_DATABASE = "dispatch_system";
